a353309590b7ca9483704fd6dd48c08ed4f0688a
Ruderson Florentino <rudersonvf@gmail.com>
2023-06-20 16:31:54 -0300

    Page home

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,29 @@
 import { NavLink } from "react-router-dom";
 import "./styles.css";
 
-export default function Header(){
-    return(
-        <header>
-            <nav>
-                <NavLink to={""}>Sobre</NavLink>
-                <NavLink to={""}>Projetos</NavLink>
-                <NavLink to={""}>Contato</NavLink>
-            </nav>
-        </header>
-    );
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <NavLink
+          className={({ isActive }) => (isActive ? "fw-700" : "fw-400")}
+          to={""}
+        >
+          Sobre
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => (isActive ? "fw-700" : "fw-400")}
+          to={""}
+        >
+          Projetos
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => (isActive ? "fw-700" : "fw-400")}
+          to={""}
+        >
+          Contato
+        </NavLink>
+      </nav>
+    </header>
+  );
 }
